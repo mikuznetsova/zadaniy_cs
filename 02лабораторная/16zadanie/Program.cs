fd23a9d994cb6e8dac156991b6582734f09a170d
mikuznetsova <kuznetsowa.margo@yandex.ru>
2017-05-21 18:22:28 +0600

    lab 2

--- 02лабораторная/16zadanie/Program.cs
+++ 02лабораторная/16zadanie/Program.cs
@@ -10,6 +10,27 @@ namespace _16zadanie
     {
         static void Main(string[] args)
         {
+            String t1, t2;
+            t1 = Console.ReadLine();
+            t2 = Console.ReadLine();
+            double x, y;
+            x = double.Parse(t1);
+            y = double.Parse(t2);
+                        if (y< 0)
+            {
+                
+                    Console.WriteLine("Значение y должно быть неотрицательным");
+                    return;
+            }
+            if ( 0>x+Math.Sqrt(y))
+            {
+                Console.WriteLine("Подкоренное выражение должно быть неотрицательным");
+                return;
+            }
+            double R;
+            R = -5 * Math.Sqrt(x+Math.Sqrt(y));
+            Console.WriteLine("{0:F4}", R);
         }
     }
+    
 }
---
+++ 02лабораторная/17zadanie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String t1, t2, t3;
+            t1 = Console.ReadLine();
+            t2 = Console.ReadLine();
+            t3 = Console.ReadLine();
+            double v1, v2,s;
+            v1 = double.Parse(t1);
+            v2 = double.Parse(t2);
+            s = double.Parse(t3);
+            if (v1 < 0)
+            {
+
+                Console.WriteLine("Значение v1 должно быть неотрицательным");
+                return;
+            }
+            if (v2 < 0)
+            {
+                Console.WriteLine("Значение v2 должно быть неотрицательным");
+                return;
+            }
+            double t;
+            t = s / (v1 + v2);
+            Console.WriteLine("{0:F4}",t);
+        }
+    }
+}
