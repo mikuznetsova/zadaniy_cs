8d15b9262c1bcbc3ce8386371fc0966abbbbc322
mikuznetsova <kuznetsowa.margo@yandex.ru>
2017-05-26 00:14:28 +0600

    lab 2  24.25.26.29

---
+++ 24zadanie/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String t1, t2, t3, t4;
+            t1 = Console.ReadLine();
+            t2 = Console.ReadLine();
+            t3 = Console.ReadLine();
+            t4 = Console.ReadLine();
+            double y,m1,m2,r;
+            y = double.Parse(t1);
+            m1 = double.Parse(t2);
+            m2 = double.Parse(t3);
+            r = double.Parse(t4);
+            if (r == 0)
+            {
+
+                Console.WriteLine("Значение r должно быть не равно нулю");
+                return;
+            }
+            
+            double R;
+            R = y * ((m1 * m2) /( r * r));
+            Console.WriteLine("{0:F4}", R);
+        }
+    }
+}
