c51e5ca9744ecc989a878abc25c3250b13a24453
mikuznetsova <kuznetsowa.margo@yandex.ru>
2017-05-27 12:58:45 +0600

    07lab

---
+++ 01zadanie/27zadanie/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t1;
+            t1 = Console.ReadLine();
+            int w, i, n;
+            w = int.Parse(t1);
+
+            i = 0;
+            n = 0;
+            if (0 <= w && w <= 20)
+            {
+                while (i < w)
+
+                {
+                    Console.Write("a");
+                    i++;
+                }
+                Console.WriteLine();
+                i = 0;
+                while (i < w)
+
+                {
+                    
+                    Console.Write("b");
+                    i++;
+                }
+                Console.WriteLine();
+                i = 0;
+                while (i < w)
+
+                {
+                    Console.Write("c");
+                    i++;
+                }
+                Console.WriteLine();
+                i = 0;
+                while (i < w)
+
+                {
+                    Console.Write("d");
+                    i++;
+                }
+                Console.WriteLine();
+                i = 0;
+                while (i < w)
+
+                {
+                    Console.Write("e");
+                    i++;
+                }
+                i = 0;
+                Console.WriteLine();
+             
+                while (i < w)
+
+                {
+                    Console.Write("f");
+                    i++;
+                }
+                Console.WriteLine();
+                i = 0;
+                while (i < w)
+
+                {
+                    Console.Write("g");
+                    i++;
+                }
+                Console.WriteLine();
+                i = 0;
+                while (i < w)
+
+                {
+                    Console.Write("h");
+                    i++;
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ 01zadanie/28zadanie/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t1;
+            t1 = Console.ReadLine();
+            int w, i, j;
+            w = int.Parse(t1);
+
+            i = 0;
+            j = 0;
+            if (w < 0 || w > 20)
+            {
+                Console.WriteLine("Значение W должно быть в интервале [0, 20]");
+                return;
+            }
+            while (j <= 9)
+            {
+                while (i < w)
+
+                {
+                    Console.Write(j);
+                    i++;
+
+                }
+                Console.WriteLine();
+                i = 0;
+                j++;
+            }
+        }          
+    }
+}
