8162403aa5813a9c6103ceee89c32003f2f2bc92
mikuznetsova <kuznetsowa.margo@yandex.ru>
2017-05-30 00:02:48 +0600

    lab2 +33

---
+++ 05лабораторная/33zadanie/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            t = int.Parse(Console.ReadLine());
+
+            if (t<0||t>60)
+            {
+                Console.Write("Значение T должно быть в интервале [0, 59]");
+                return;
+            }
+            if (t%4==0||t%5==0||t%9==0||t%10==0)
+            {
+                Console.Write("Красный");
+            }
+            else
+            {
+                Console.Write("Зеленый");
+            }
+        }
+    }
+}
---
+++ 05лабораторная/34zadanie/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w,h,c,d, r;
+            w = double.Parse(Console.ReadLine());
+           h = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            d = double.Parse(Console.ReadLine());
+            
+            if (w<=0)
+            {
+                Console.Write("Значение W должно быть положительным");
+                return;
+            }
+            if (h <= 0)
+            {
+                Console.Write("Значение H должно быть положительным");
+                return;
+            }
+            if (c <= 0)
+            {
+                Console.Write("Значение C должно быть положительным");
+                return;
+            }
+            if (h <= 0)
+            {
+                Console.Write("Значение D должно быть положительным");
+                return;
+            }
+            r =(w*h)/(c*d);
+            r=r
+                if (w / c >= h / c)
+            {
+                Console.Write(r + "при C вдоль W");
+                
+            }
+            else 
+             {
+                Console.Write(r + "при C вдоль H");
+            }
+        }
+    }
+}
