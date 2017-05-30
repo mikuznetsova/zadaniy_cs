b7021d795ba0c1b35d03ef512b09244934b17cc4
mikuznetsova <kuznetsowa.margo@yandex.ru>
2017-05-30 20:25:00 +0600

    8lab

--- 05лабораторная/31zadanie/Program.cs
+++ 05лабораторная/31zadanie/Program.cs
@@ -15,29 +15,35 @@ namespace _31zadanie
             b1= double.Parse(Console.ReadLine());
             a2 = double.Parse(Console.ReadLine());
             b2 = double.Parse(Console.ReadLine());
-           
+            bool intersectX = false;
             if (a1>b1||a2>b2)
             {
                 Console.Write("Значение A должно быть меньше значения B");
                 return;
             }
-                        
-            if (a1<=a2&&a2<=b1  && b2>b1)
+            if (a1 <= a2)
             {
-                Console.Write("пересекаются ("+a2+";"+b1+")");
+                if (a2 <= b1 && b2 > b1)
+                {
+                    Console.Write("пересекаются (" + a2 + ";" + b1 + ")");
+                    intersectX = true;
+                }
+                if (a2 <= b1 && b2 <= b1)
+                {
+                    Console.Write("пересекаются (" + a2 + ";" + b2 + ")");
+                    intersectX = true;
+                }
             }
-            if (a1 <= a2 && a2 <= b1 && b2 <= b1)
-            {
-                Console.Write("пересекаются (" + a2 + ";" + b2+ ")");
-            }
-            if (a1 <= b2 && b2 <= b1 && a2 < a1)
-            {
-                Console.Write("пересекаются (" + a1 + ";" + b2 + ")");
-            }
-            
             else
             {
-                Console.Write("не пересекаются");
+                if (a1 <= b2 && b2 <= b1)
+                {
+                    Console.Write("пересекаются (" + a1 + ";" + b2 + ")");
+                }
+                else
+                {
+                    Console.Write("не пересекаются");
+                }
             }
         }
     }
