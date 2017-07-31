5d27a3204903c1ef09bd3c4c0a65eff6c1309c98
akryukov92 <akryukov992@gmail.com>
2017-07-31 14:18:23 +0700

    Добавлены материалы для ЛР14

---
+++ Lab12/Classes/task4147/Rectangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.task4147
+{
+    public class Rectangle
+    {
+
+        private int x;
+        private int y;
+        private int w;
+        private int h;
+
+        public Rectangle(int x, int y, int w, int h)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+
+        public Rectangle(Point p, int w, int h)
+        {
+            this.x = p.X;
+            this.y = p.Y;
+            this.w = w;
+            this.h = h;
+        }
+
+        public Rectangle(Interval rangeX, Interval rangeY)
+        {
+            this.x = rangeX.GetA();
+            this.w = rangeX.Length();
+            this.y = rangeY.GetA();
+            this.h = rangeY.Length();
+        }
+
+        public bool contains(Point p)
+        {
+            return x <= p.X && p.X <= x + w
+                && y <= p.Y && p.Y <= y + h;
+        }
+
+        public double area()
+        {
+            return w * h;
+        }
+
+        public Interval getProjectionX()
+        {
+            return new Interval(x, x + w);
+        }
+
+        public Interval getProjectionY()
+        {
+            return new Interval(y, y + h);
+        }
+
+        public override String ToString()
+        {
+            return "<rect x='" + x +
+                "' y='" + y +
+                "' w='" + w +
+                "' h='" + h +
+                "'/>";
+        }
+    }
+}
